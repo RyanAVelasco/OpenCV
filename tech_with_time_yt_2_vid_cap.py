0cfd1c1810d63ebc61d013d2c93e3b70d8ab6524
RyanAVelasco <45979367+RyanAVelasco@users.noreply.github.com>
2021-09-24 00:08:33 -0500

    Create tech_with_time_yt_2_vid_cap.py

---
+++ tech_with_time_yt_2_vid_cap.py
@@ -0,0 +1,8 @@
+import cv2 as cv
+import numpy as py
+
+# This is following tutorial #3 of https://www.youtube.com/watch?v=rKcwcARdg9M
+# 00:00 | Introduction
+# 00:50 | Displaying video capture device
+# 07:11 | Mirroring video multiple times
+
